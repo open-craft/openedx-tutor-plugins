621ea9586648d5f894f015d4ad5fec6fea355ca4
Kaustav Banerjee <kaustav@opencraft.com>
2025-05-19 21:31:19 +0530

    fix: use correct imports

--- plugins/tutor-contrib-scout-apm/tutor_scout_apm/plugin.py
+++ plugins/tutor-contrib-scout-apm/tutor_scout_apm/plugin.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-from glob import glob
 import os
-import pkg_resources
-import uuid
+from importlib import resources
 
 from tutor import hooks as tutor_hooks
 
@@ -35,12 +33,7 @@ tutor_hooks.Filters.CONFIG_DEFAULTS.add_item(
 
 # For each file in tutor_media/patches,
 # apply a patch based on the file's name and contents.
-patch_files = glob(
-    os.path.join(
-        pkg_resources.resource_filename("tutor_scout_apm", "patches"),
-        "*",
-    )
-)
-for path in patch_files:
+patches = resources.files("tutor_scout_apm") / "patches"
+for path in patches.glob("*"):
     with open(path, encoding="utf-8") as patch_file:
         tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
